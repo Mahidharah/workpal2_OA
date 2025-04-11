add31b757add75d707bd4e5edb47f601ac223224
C Mahidharah Rajendran <mahidharah@gmail.com>
2025-04-11 02:04:28 -0400

    Update index.js

--- workpal-backend/src/routes/index.js
+++ workpal-backend/src/routes/index.js
@@ -4,14 +4,22 @@ const router = express.Router();
 const TeacherController = require('../controllers/teacherController');
 const RegisterController = require('../controllers/registerController');
 const StudentController = require('../controllers/studentController.js');
+const NotificationController = require('../controllers/notificationController');
 
-// Define the route to get all teachers
+
+// Endpoint to get all teachers
 router.get('/api/teachers', TeacherController.getAll);
 
-// Route to register students to a teacher
+// Endpoint to register students to a teacher
 router.post('/api/register', RegisterController.registerStudents);
 
 // Endpoint to get common students
 router.get('/api/commonstudents', StudentController.getCommonStudents);
+
+// Endpoint to suspend a student  
+router.post('/api/suspend', StudentController.suspendStudent);
+
+// Endpoint to retrieve notification recipients after creating a notification
+router.post('/api/retrievefornotifications', NotificationController.retrieveForNotifications);
 
 module.exports = router;
